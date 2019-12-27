ecf1ab6051a1d2a6f688aa98135b72613b2d8886
Leon <leonronssin@appnovation.com>
2019-12-27 15:37:17 -0500

    display name prompt

--- src/store/modules/players.js
+++ src/store/modules/players.js
@@ -13,13 +13,11 @@ export default {
   },
   actions: {
     getRow({ commit }, uid) {
-      if (!uid) return 
+      if (!uid) return
       window.db.collection("users").doc(uid)
         .get().then(function(doc) {
           if (doc.exists) {
             commit('addRow', { uid, row: doc.data() })
-          } else {
-            window.console.log("No such user!");
           }
         }).catch(function(error) {
           window.console.log("Error getting document:", error);
